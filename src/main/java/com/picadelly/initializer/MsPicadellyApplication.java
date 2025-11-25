f543cb3c8dd4fe9be6ca6046eac1960f54ab0d37
Camilo Garcia <juan092005ca@gmail.com>
2025-11-25 16:24:25 -0500

    First Commit
    Creacion de la estructura basica del proyecto

---
+++ src/main/java/com/picadelly/initializer/MsPicadellyApplication.java
@@ -0,0 +1,13 @@
+package com.picadelly.initializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPicadellyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPicadellyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/picadelly/initializer/MsPicadellyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picadelly.initializer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPicadellyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
